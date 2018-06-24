f6776455dbe57eaabb2e833ec68e9af2ea18a1f0
Marc Ribalta Gene <mariva1995@gmail.com>
2018-06-24 23:41:01 +0200

    right now, it works, appending to sheet1

---
+++ index.js
@@ -0,0 +1,28 @@
+let google = require('googleapis');
+let authentication = require("./authentication");
+
+
+function appendData(auth) {
+  var sheets = google.sheets('v4');
+  sheets.spreadsheets.values.append({
+    auth: auth,
+    spreadsheetId: '1tl-wWFz-SSUvi5Ug1h3-q3eWN4VgzVu9K-85M1ITmlc',
+    range: 'A:E', //Change Sheet1 if your worksheet's name is something else
+    valueInputOption: 'RAW',
+    insertDataOption: 'INSERT_ROWS',
+    resource: {
+      values: [ ["Void", "Canvas", "Website"], ["Paul", "Shan", "Human"] ]
+    }
+  }, (err, response) => {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    } else {
+        console.log("Appended");
+    }
+  });
+}
+
+authentication.authenticate().then((auth)=>{
+    appendData(auth);
+});
